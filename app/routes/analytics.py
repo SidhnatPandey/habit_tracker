5d47e1d16479b9667ac080714c766fb2c1abee14
Sidhant Pandey <sidhantpandeynp6@gmail.com>
2025-08-31 09:26:03 +0530

    feature main :
    * Change remove unused code from analytics

--- app/routes/analytics.py
+++ app/routes/analytics.py
@@ -1,25 +1,3 @@
-# from fastapi import APIRouter, Depends
-# from sqlalchemy.orm import Session
-# from datetime import date
-# from .. import crud, database
-
-# router = APIRouter(prefix="/analytics", tags=["Analytics"])
-
-# @router.get("/habit/{habit_id}")
-# def habit_analytics(habit_id: int, start: date, end: date, db: Session = Depends(database.get_db)):
-#     """
-#     Example: /analytics/habit/1?start=2025-01-01&end=2025-01-31
-#     """
-#     return crud.get_habit_analytics(db, habit_id, start, end)
-
-# @router.get("/top")
-# def top_habits(start: date, end: date, db: Session = Depends(database.get_db)):
-#     """
-#     Example: /analytics/top?start=2025-01-01&end=2025-01-31
-#     """
-#     return crud.top_3_habits(db, start, end)
-
-
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import models, database
